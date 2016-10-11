3dc6d15582a669163c6fbc18088abcac66bdfdc8
Ruicong Xie <rxie25@gmail.com>
2016-10-11 17:46:37 -0400

    setup dev env added login REST API

---
+++ src/README.md
@@ -0,0 +1,11 @@
+active env
+`source venv/bin/activate`
+
+deactivate env
+`deactivate`
+
+install required packages
+`pip install -r requirements.txt`
+
+install package and save it to requirements.txt
+`pip install package && pip freeze > requirements.txt`
---
+++ src/server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, session, jsonify
+from flask_pymongo import PyMongo
+import bcrypt
+
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'cloudlogin'
+app.config['MONGO_URI'] = 'mongodb://user:user@ds055626.mlab.com:55626/cloudlogin'
+
+mongo = PyMongo(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    print users
+    login_user = users.find_one({'username' : request.form['username']})
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return jsonify({'status': 200})
+            # login successful
+
+    return jsonify({'status': 400})
+    # login failed
+
+@app.route('/register', methods=['POST'])
+def register():
+    users = mongo.db.users
+    existing_user = users.find_one({'name' : request.form['username']})
+    if existing_user is None:
+        hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        users.insert({'username' : request.form['username'], 'password' : hashpass})
+        session['username'] = request.form['username']
+        return jsonify({'status': 202})
+        # registraton accepted
+
+    return jsonify({'status': 404})
+    # user already exist
+
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
